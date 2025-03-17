ff803d54621326d856e31fe1301ac145ddf88e27
Sonia Serrano Lorenzo <soniaserranolorenzo@gmail.com>
2025-03-16 19:56:54 -0700

    Added printing messages when errors occur

--- program.py
+++ program.py
@@ -1,29 +1,88 @@
-from helper_functions import (
+from src.helper_functions import (
     parse_lookup_table,
     parse_flow_log_records,
     generate_output_file,
 )
 
-csv_file_name = "../lib/lookup-table.csv"
-flow_log_records_file_name = "../lib/sample-flow-logs-v2.txt"
+TEST_CSV_FILE_PATH = "./lib/lookup_table.csv"
+TEST_TXT_FILE_PATH = "./lib/sample_flow_log_records.txt"
 
 
-def runProgram(csv_file_name: str, flow_log_records_file_name: str) -> None:
+def countTagAndPortProtocolCombinations(
+    lookup_table_file_path: str, flow_log_records_file_path: str
+) -> None:
+    """
+    Creates a text file with tag and port/protocol combinations count by using
+    the helper functions parse_look_table, parse_flow_log_records, and generate_output_file.
+    Prints error messages if errors occur while extracting data.
+
+    :params lookup_table_file_path: relative file path to file with lookup table data
+    :params flow_log_records_file_name: relative file path to file with flow log records data
+    :returns: None
+    """
     try:
-        lookup_table = parse_lookup_table(csv_file_name)
-    except:
-        print("No values parsed from lookup table")
+        lookup_table = parse_lookup_table(lookup_table_file_path)
+    except IsADirectoryError:
+        print("⚠️ Lookup table file path is a directory")
+        return
+    except FileNotFoundError:
+        print("⚠️ No file found at lookup table file path")
+        return
+    except ValueError:
+        print("⚠️ Lookup table file is not a CSV file")
+        return
+    except OSError:
+        print("⚠️ Error occurred while reading lookup table file")
+    except Exception:
+        print("⚠️ Unexpected error occurred while parsing lookup table file")
+        return
+
+    if not lookup_table:
+        print("⚠️ No or invalid lookup table data")
         return
 
     try:
-        flow_log_records = parse_flow_log_records(flow_log_records_file_name)
+        flow_log_records = parse_flow_log_records(flow_log_records_file_path)
+    except IsADirectoryError:
+        print("⚠️ Flow log records file path is a directory")
+        return
+    except FileNotFoundError:
+        print("⚠️ No file found at flow log records file path")
+        return
+    except ValueError:
+        print("⚠️ Flow log records file is not a CSV file")
+        return
+    except OSError:
+        print("⚠️ Error occurred while reading flow log records file")
+    except Exception:
+        print("⚠️ Unexpected error occurred while parsing flow log records file")
+        return
 
-    except:
-        print("No valid flow log records")
+    if not flow_log_records:
+        print("⚠️ No or invalid flow log records data")
         return
 
     generate_output_file(lookup_table, flow_log_records)
-    print("Created output.txt file in project directory")
+    print("✅ Created output.txt in program directory")
 
 
-runProgram(csv_file_name, flow_log_records_file_name)
+def getFilePaths():
+    csv_file_path = input("Enter relative file path to lookup table csv file: ")
+
+    if not csv_file_path:
+        print("⚠️ No csv file path entered")
+        return
+
+    txt_file_path = input("Enter relative file path to flow log records txt file: ")
+
+    if not txt_file_path:
+        print("⚠️ No txt file path entered")
+        return
+
+    countTagAndPortProtocolCombinations(csv_file_path, txt_file_path)
+
+
+# TEST
+# countTagAndPortProtocolCombinations(TEST_CSV_FILE_PATH, TEST_TXT_FILE_PATH)
+
+getFilePaths()
